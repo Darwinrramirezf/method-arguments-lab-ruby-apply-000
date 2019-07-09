6d23e08f7d5f6444be34857f0da0f46fdf92911e
Darwin Ramirez <darwinrramirezf@gmail.com>
2019-07-09 17:46:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,12 @@
 # The introduction_with_language Method
 
-def introduction_with_language(name = "darwin", language = "ruby")
+def introduction_with_language(name, language)
 
 puts ("Hi, my name is #{name} and I'm learning to program in #{language}.").to_stdout
 
 end
 
-def introduction_with_language2(name = "darwin", language = "ruby")
+def introduction_with_language2(name, language)
 
 puts ("Hi, my name is #{name} and I'm learning to program in #{language}.")
 end
