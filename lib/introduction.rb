7bc188e3d0d7b2ebe9e437fe4bb771078939181d
Darwin Ramirez <darwinrramirezf@gmail.com>
2019-07-09 17:57:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,12 +7,12 @@ puts "Hi, my name is #{name}.\n".to_stdout
 end
 
 
-
+=begin
 def introduction_with_language(name, language)
 
 puts ("Hi, my name is #{name} and I'm learning to program in #{language}.").to_stdout
 
 end
-
+=end
 
 
